2af6fa40dcf2e0b056523a68640ec0930c55cd55
zlemisie <zlemisie@gmail.com>
2014-12-26 19:08:19 +0100

    presenting tweets with Angular

--- src/main/java/demo/TwitterEndpoint.java
+++ src/main/java/demo/TwitterEndpoint.java
@@ -1,11 +1,12 @@
 package demo;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.springframework.social.connect.ConnectionRepository;
-import org.springframework.social.twitter.api.CursoredList;
+import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
-import org.springframework.social.twitter.api.TwitterProfile;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,14 +21,12 @@ public class TwitterEndpoint {
     private ConnectionRepository connectionRepository;
 
     @RequestMapping("/twitter")
-    public CursoredList<TwitterProfile> helloTwitter(Model model) {
+    public List<Tweet> helloTwitter(Model model) {
 //        if (connectionRepository.findPrimaryConnection(Twitter.class) == null) {
 //            return "redirect:/connect/twitter";
 //        }
 
-        model.addAttribute(twitter.userOperations().getUserProfile());
-        CursoredList<TwitterProfile> friends = twitter.friendOperations().getFriends();
-        model.addAttribute("friends", friends);
-        return friends;
+
+        return twitter.timelineOperations().getHomeTimeline();
     }
 }
